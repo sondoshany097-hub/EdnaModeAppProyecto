a6fefec61cd519e0631224b14e3812161a6688b7
celia hany <sondoshany097@gmail.com>
2026-03-24 13:47:58 +0100

    pantalla de login

--- Java/src/vista/MainView.java
+++ Java/src/vista/MainView.java
@@ -9,5 +9,9 @@ import javax.swing.JFrame;
  * 
  */
 public class MainView extends JFrame {
+	public static void main(String[] args) {
+	    new login_view("Login - Edna Moda").setVisible(true);
+	}
+	
 
 }
